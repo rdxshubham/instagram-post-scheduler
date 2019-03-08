a19c0cee77b618682f7759bbd33e182a7cc152e2
Shubham Gupta <shubhamgupta@my.smccd.edu>
2019-03-08 19:54:26 +0530

    status check added

--- app.py
+++ app.py
@@ -11,3 +11,11 @@ random_image_to_upload = Config.IMAGE_FOLDER_PATH + list_of_images[randrange(0, 
 random_caption = Config.CAPTION_LIST[randrange(0, len(Config.CAPTION_LIST))]
 
 InstagramAPI.uploadPhoto(random_image_to_upload, random_caption)
+status = InstagramAPI.LastResponse
+
+for j in status:
+    if j['status'] == 'ok':
+        print('Uploaded Successfully')
+        os.remove(random_image_to_upload)
+    else:
+        print('Some error happened -> ' + j)

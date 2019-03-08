c22533d26b5bcfb648caf01f69aea09c13896d71
Shubham Gupta <shubhamgupta@my.smccd.edu>
2019-03-08 20:26:16 +0530

    configurations added

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 
 class Config:
 
-    USERNAME = 'suchitagoshal'
-    PASSWORD = 'dummy'
+    USERNAME = 'USERNAME'
+    PASSWORD = 'PASSWORD'
     IMAGE_FOLDER_PATH = os.getcwd() + '/images/'
     CAPTION_LIST = ['Hello', 'Its fun loving']
+    INTERVAL_TIME_IN_HOURS = 1

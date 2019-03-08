3578015aa6df527ad42443d920f631428d3f9009
Shubham Gupta <shubhamgupta@my.smccd.edu>
2019-03-08 19:18:43 +0530

    Config file added

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+
+    USERNAME = 'suchitagoshal'
+    PASSWORD = 'dummy'
+    IMAGE_FOLDER_PATH = os.getcwd() + '/images/'
+    CAPTION_LIST = ['Hello', 'Its fun loving']

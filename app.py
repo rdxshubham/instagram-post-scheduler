33b0b9764be3fddf36a2047fab66305ce8750b7d
Shubham Gupta <shubhamgupta@my.smccd.edu>
2019-03-08 20:25:54 +0530

    apscheduler added

--- app.py
+++ app.py
@@ -1,21 +1,44 @@
-from InstagramAPI import InstagramAPI
 from config import Config
 import os
 from random import randrange
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+import atexit
+from time import sleep
 
-InstagramAPI = InstagramAPI(Config.USERNAME, Config.PASSWORD)
-InstagramAPI.login()
 
-list_of_images = os.listdir(Config.IMAGE_FOLDER_PATH)
-random_image_to_upload = Config.IMAGE_FOLDER_PATH + list_of_images[randrange(0, len(list_of_images))]
-random_caption = Config.CAPTION_LIST[randrange(0, len(Config.CAPTION_LIST))]
+def UploadPhoto():
 
-InstagramAPI.uploadPhoto(random_image_to_upload, random_caption)
-status = InstagramAPI.LastResponse
+    from InstagramAPI import InstagramAPI
 
-for j in status:
-    if j['status'] == 'ok':
+    InstagramAPI = InstagramAPI(Config.USERNAME, Config.PASSWORD)
+    InstagramAPI.login()
+
+    list_of_images = os.listdir(Config.IMAGE_FOLDER_PATH)
+    random_image_to_upload = Config.IMAGE_FOLDER_PATH + list_of_images[randrange(0, len(list_of_images))]
+    random_caption = Config.CAPTION_LIST[randrange(0, len(Config.CAPTION_LIST))]
+
+    InstagramAPI.uploadPhoto(random_image_to_upload, random_caption)
+    status = InstagramAPI.LastJson.get('status')
+    print(status)
+
+    if status == 'ok':
         print('Uploaded Successfully')
         os.remove(random_image_to_upload)
     else:
-        print('Some error happened -> ' + j)
+        print('Some error happened -> ' + status)
+
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=UploadPhoto,
+    trigger=IntervalTrigger(hours=Config.INTERVAL_TIME_IN_HOURS),
+    id='upload_photo',
+    name='upload photo',
+    replace_existing=True)
+
+atexit.register(lambda: scheduler.shutdown())
+
+while True:
+    sleep(1)
